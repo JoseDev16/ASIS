b1fd0b3aa689dd74bf88f64de8d2784a66aeba53
rubper <rub.per96@gmail.com>
2019-07-12 14:07:05 -0600

    detalles a controlvacuna + desparasitante

--- SIASIS/apps/controlDesp/urls.py
+++ SIASIS/apps/controlDesp/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, re_path
-from apps.controlDesp.views import index_desp, registrar_desp, editar_desp, eliminar_desp, nuevo_desp,index_desp3
+from apps.controlDesp import views
 from django.contrib.auth.decorators import login_required
+
 app_name = "controlDesp"
 
 urlpatterns = [
-	path('', index_desp3, name="index_desp3"),
-	path('index_desp/<int:x>', index_desp, name="index_desp2"),
-    path('registrar_desp/', registrar_desp, name="registrar_desp"),
-    path('editar_desp/<int:num>', editar_desp, name="editar_desp"),
-    path('eliminar-mascota/<int:num>',eliminar_desp,name="eliminar_desp"),
-    path('nuevo_desp/',nuevo_desp,name="nuevo_desp"),
-    
+	path('<int:x>', (views.index_desparasitacion), name="index_desp2"),
+    path('index-desparasitacion/<int:x>', (views.index_desparasitacion),name="index-desparasitacion"),	
+    path('registrar-vacuna-desparasitacion/<int:x>',(views.registrar_vacuna),name="registrar-desparasitacion"),
+    path('editar-desparasitacion/<int:x>/<int:num>', (views.editar_desparasitacion), name="editar-desparasitacion"),
+    path('eliminar-desparasitacion/<int:x>/<int:num>', (views.eliminar_desparasitacion), name="eliminar-desparasitacion"),
 ]
