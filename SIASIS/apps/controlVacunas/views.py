0668f314df16033830b51c4d04703f886f0c90bc
rubper <rub.per96@gmail.com>
2019-07-12 13:16:24 -0600

    form para registrar vacuna en index + editar + eliminar + fecha proxima vacuna + clases bootstrap

--- SIASIS/apps/controlVacunas/views.py
+++ SIASIS/apps/controlVacunas/views.py
@@ -13,8 +13,8 @@ from apps.registroMascota.models import Expediente
 def index_vacu(request,x):
     edicion = False
     vacu = ControlVacuna.objects.filter(expediente=x).order_by('id')
+    ultVacu = ControlVacuna.objects.filter(expediente=x).order_by('id').last()
     expediente=Expediente.objects.get(id=x)
-    
     if request.method == "POST":  
         form = ControlVacuForm(request.POST)  
         if form.is_valid():  
@@ -22,63 +22,61 @@ def index_vacu(request,x):
                 instance = form.save(commit=False) 
                 instance.expediente = expediente
                 instance.save()
-                return HttpResponseRedirect(reverse('controlVacunas:index_vacu', kwargs={'x':x}))  
+                return HttpResponseRedirect(reverse('controlVacunas:index_vacu2', kwargs={'x':x}))  
             except:  
                 pass  
     else:  
         form = ControlVacuForm()   
-    return render(request,'controlVacunas/index_vacuna.html',{'varvacu':vacu,'form':form, 'edicion':edicion})
+    return render(request,'controlVacunas/index_vacuna.html',{'varvacu':vacu,'form':form, 'edicion':edicion, 'idmasc':expediente,'ultVacu':ultVacu})
 
 def registrar_vacu(request):
 	if request.method == 'POST':
 		form=ControlVacuForm(request.POST)
 		if form.is_valid():
 			form.save()
-		return redirect('controlVacunas:index_vacu')
+		return redirect(reverse('controlVacunas:index_vacu2',kwargs={'x':x}))
 	else:
 		form=ControlVacuForm()
 	return render(request,'controlVacunas/registrar_vacuna.html',{'form':form})
 
-def editar_vacu(request, num):
-	vacuna=ControlVacuna.objects.get(id=num)
-	if request.method=='GET':
-		form=ControlVacuForm(instance=vacuna)
-	else:
-		form=ControlVacuForm(request.POST, instance=vacuna)
-		if form.is_valid():
-			form.save()
-			return redirect('controlVacunas:index_vacu3')
-	return render(request,'controlVacunas/registrar_vacuna.html', {'form':form})
+def editar_vacu(request, x, num):
+    edicion = True
+    vacunas = ControlVacuna.objects.filter(expediente=x).order_by('id')
+    instancia = ControlVacuna.objects.get(id=num)
+    expediente=Expediente.objects.get(id=x)
+    if request.method=='GET':
+        form=ControlVacuForm(instance=instancia)
+    else:
+        form=ControlVacuForm(request.POST, instance=instancia)
+        if form.is_valid():
+            form.save()
+            form = ControlVacuForm()
+            return redirect(reverse('controlVacunas:index_vacu2',kwargs={'x':x}))
+    return render(request,'controlVacunas/index_vacuna.html', {'varvacunas':vacunas,'form':form,'edicion':edicion,'idmasc':expediente})
 
-def eliminar_vacu(request, num):  
-    vacu = ControlVacuna.objects.get(id=num) 
-    if request.method=='POST':
-    	vacu.delete()  
-    	return redirect('controlVacunas:index_vacu3')
-    return render(request,'controlVacunas/eliminar_vacuna.html',{'vacu':vacu})
+def eliminar_vacu(request, x, num):  
+    vacu = ControlVacuna.objects.filter(expediente=x).get(id=num) 
+    vacu.delete()  
+    return redirect(reverse('controlVacunas:index_vacu2',kwargs={'x':x}))
 
-def nueva_vacu(request):
+def nueva_vacu(request,x):
     if request.method=='POST':
         form=VacuForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('controlVacunas:index_vacu3')
+        return redirect(reverse('controlVacunas:index_vacu2',kwargs={'x':x}))
     else:
         form=VacuForm()
-    return render(request, 'controlVacunas/nueva_vacu.html', {'form':form})
+    return render(request, 'controlVacunas/nueva_vacu.html', {'form':form,'idmasc':x})
 
 def index_vacu3(request):
     edicion = False
     vacu = ControlVacuna.objects.all().order_by('id')
-    
-    
     if request.method == "POST":  
         form = ControlVacuForm(request.POST)  
         if form.is_valid():  
             try:
-                form.save()                  
-
-
+                form.save()           
                 return HttpResponseRedirect(reverse('controlVacunas:index_vacu3'))  
             except:  
                 pass  
