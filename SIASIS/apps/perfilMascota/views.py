6410ae1abaccfddf7cee928e5f32e6b4d46e2e2b
rubper <rub.per96@gmail.com>
2019-07-12 00:36:40 -0600

    perfil mascota + subida de imagen de perfil

--- SIASIS/apps/perfilMascota/views.py
+++ SIASIS/apps/perfilMascota/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from apps.registroMascota.models import Mascota
 from apps.registroMascota.models import Expediente
+from apps.perfilMascota.models import imagenPerfil
 from apps.controlCelo.models import ControlCelo
 from apps.controlConsultas.models import Consulta
 from apps.controlDesp.models import ControlDesparasitacion
 from apps.controlVacunas.models import ControlVacuna
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 def perfil_mascota(request,id):
     expediente = Expediente.objects.get(id=id)
@@ -31,6 +35,21 @@ def perfil_mascota(request,id):
         'consulta':consulta,
         'desp':desparasitacion,
         'vacuna':vacuna,
+        'id':id
     }
+    if request.method == 'POST' and request.FILES['pr-pic-'+str(id)]:
+        imagenPerf = request.FILES['pr-pic-'+str(id)]
+        fs=FileSystemStorage()
+        archivo = fs.save('pr-pic-'+str(id)+'.jpg', imagenPerf)
+        url_imagen = fs.url(archivo)
+        return render(request, 'perfilMascota/perfil_mascota.html', {
+            'mascota':mascota,
+            'celo':celo,
+            'consulta':consulta,
+            'desp':desparasitacion,
+            'vacuna':vacuna,
+            'imagenPerfil':url_imagen,
+            'id':id
+        })
     return render(request,'perfilMascota/perfil_mascota.html',context)
     # Create your views here.
